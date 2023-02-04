6419ceac574ba7cc47a60b0479a0fdec947a8dcc
cavalcantinetto <cavalcantinetto@hotmail.com>
2023-02-04 17:19:20 -0300

    rebased - techer class modified

--- models/teacher.js
+++ models/teacher.js
@@ -20,6 +20,10 @@ const contactSchema = new mongoose.Schema({
     "teacherLevel": {
         type: String,
         required: true
+    },
+    "teacherClass": {
+        type: String,
+        required: true
     }
 
 })
